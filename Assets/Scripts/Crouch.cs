4cfa1999956141c1624d269e1e5ef9e13cfdadd4
MartinMcK <33032026+MartinMcK@users.noreply.github.com>
2019-01-16 04:00:43 +0000

    Lighting updated

---
+++ Assets/Scripts/Crouch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crouch : MonoBehaviour
+{
+
+    public CharacterController characterController;
+    // Use this for initialization
+    void Start()
+    {
+        characterController = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.C))
+        {
+            characterController.height = 0.4f; //Crouch jsut lowers the chracters height 
+        }
+        else
+        {
+            characterController.height = 1.0f; //puts characters height to normal is C isnt pressed down 
+        }
+    }
+}
