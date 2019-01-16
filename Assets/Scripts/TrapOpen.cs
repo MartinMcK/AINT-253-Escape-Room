9eaeb58dab0dddf666d658664c29a8aaaacf65c9
MartinMcK <33032026+MartinMcK@users.noreply.github.com>
2019-01-16 18:01:09 +0000

    third puzzle

---
+++ Assets/Scripts/TrapOpen.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapOpen : MonoBehaviour {
+
+    public GameObject TrapDoor;
+
+    private void OnTriggerEnter()
+    {
+        TrapDoor.GetComponent<Animation>().Play("TrapAnimation");
+
+    }
+}
